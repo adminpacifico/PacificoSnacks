27fb78a68e53c698f4113a9a270cd6119750b632
adminpacifico <56931073+adminpacifico@users.noreply.github.com>
2021-07-08 20:40:32 -0500

    actualizacion nomina

--- hr_vacations_report_etet_v1/models/vacations_report.py
+++ hr_vacations_report_etet_v1/models/vacations_report.py
@@ -14,6 +14,10 @@ import types
 import logging
 import time
 _logger = logging.getLogger(__name__)
+from dateutil.relativedelta import relativedelta
+
+def days360(s, e):
+    return ( ((e.year * 12 + e.month) * 30 + e.day) - ((s.year * 12 + s.month) * 30 + s.day))
 
 class VacationsReport(models.TransientModel):
     _name = 'vacations.report'
@@ -44,11 +48,11 @@ class VacationsReport(models.TransientModel):
         _logger.error("INICIA LA FUNCIÓN CONSTRUIR EL ARCHIVO ")
 
         if self.modo == '1':
-            contratos = self.env['hr.contract'].search([('employee_id', '=', self.empleado.id)])
+            contratos = self.env['hr.contract'].search([('employee_id', '=', self.empleado.id), ('state', '=', 'open')])
         elif self.modo == '2':
-            contratos = self.env['hr.contract'].search([('department_id', '=', self.departamento.id), ('employee_id', '!=', False)])
+            contratos = self.env['hr.contract'].search([('department_id', '=', self.departamento.id), ('employee_id', '!=', False), ('state', '=', 'open')])
         else:
-            contratos = self.env['hr.contract'].search([('employee_id', '!=', False)])
+            contratos = self.env['hr.contract'].search([('employee_id', '!=', False), ('state', '=', 'open')])
 
         date_creation = fields.Date.today()
         hora = time.strftime('%H:%M:%S')
@@ -60,8 +64,11 @@ class VacationsReport(models.TransientModel):
         ws = wb.add_worksheet('Report')
         ws.set_column('A:A', 15)
         ws.set_column('B:B', 65)
-        ws.set_column('C:G', 23)
-        ws.set_column('H:Z', 18)
+        ws.set_column('C:D', 23)
+        ws.set_column('E:E', 28)
+        ws.set_column('F:G', 23)
+        ws.set_column('H:K', 18)
+        ws.set_column('L:Z', 23)
 
         # formatos
         title_head = wb.add_format({
@@ -98,6 +105,18 @@ class VacationsReport(models.TransientModel):
             ws.write(fila, 8, 'DÍAS NO HÁBILES', title_head)
             ws.write(fila, 9, 'DÍAS TOTALES', title_head)
             ws.write(fila, 10, 'VALOR PAGADO', title_head)
+            #ws.write(fila, 11, 'HORAS EXTRA DIURNA PROMEDIO 12M', title_head)
+            #ws.write(fila, 12, 'HORAS EXTRA DIURNA FESTIVO PROMEDIO 12M', title_head)
+            #ws.write(fila, 13, 'HORAS EXTRA NOCTURNA PROMEDIO 12M', title_head)
+            #ws.write(fila, 14, 'HORAS EXTRA NOCTURNA FESTIVO PROMEDIO 12M', title_head)
+            #ws.write(fila, 15, 'HORAS RECARGO NOCTURNO PROMEDIO 12M', title_head)
+            #ws.write(fila, 16, 'HORAS RECARGO DIURNO FESTIVO PROMEDIO 12M', title_head)
+            #ws.write(fila, 17, 'HORAS RECARGO NOCTURNO FESTIVO PROMEDIO 12M', title_head)
+            #ws.write(fila, 18, 'BONIFICACION PROMEDIO 12M', title_head)
+            #ws.write(fila, 19, 'COMISION PROMEDIO 12M', title_head)
+            #ws.write(fila, 20, 'SALARIO CONTRATO', title_head)
+            #ws.write(fila, 21, 'SALARIO PROMEDIO 12M', title_head)
+            ws.write(fila, 11, 'SALARIO PROMEDIO 12M', title_head)
 
             fila += 1
 
@@ -118,16 +137,13 @@ class VacationsReport(models.TransientModel):
                 for vac in cont.vacations_history:
                     ws.write(fila, 5, '') if not vac.request_date_from else ws.write(fila, 5, vac.request_date_from, format_date)
                     ws.write(fila, 6, '') if not vac.request_date_to else ws.write(fila, 6, vac.request_date_to, format_date)
-                    #
-                    dias_totales = ((vac.request_date_to - vac.request_date_from).days) + 1
-                    dias_no_habiles = dias_totales - vac.number_of_days
 
-                    ws.write(fila, 7, 0) if not vac.name else ws.write(fila, 7, vac.number_of_days)
-                    total_dias_habiles += int(vac.number_of_days)
-                    ws.write(fila, 8, 0) if not vac.name else ws.write(fila, 8, dias_no_habiles)
-                    total_dias_no_habiles += int(dias_no_habiles)
-                    ws.write(fila, 9, 0) if not vac.name else ws.write(fila, 9, dias_totales)
-                    total_dias += int(dias_totales)
+                    ws.write(fila, 7, 0) if not vac.name else ws.write(fila, 7, vac.workday)
+                    total_dias_habiles += int(vac.workday)
+                    ws.write(fila, 8, 0) if not vac.name else ws.write(fila, 8, vac.holiday)
+                    total_dias_no_habiles += int(vac.holiday)
+                    ws.write(fila, 9, 0) if not vac.name else ws.write(fila, 9, vac.number_of_days)
+                    total_dias += int(vac.number_of_days)
                     ws.write(fila, 10, 0) if not vac.name else ws.write(fila, 10, vac.amount_vacations, format_number)
                     total_valor_pagado += vac.amount_vacations
 
@@ -136,6 +152,7 @@ class VacationsReport(models.TransientModel):
 
             if not cont.vacations_history:
                 fila += 1
+
             ws.write(fila, 0, '', title_head)
             ws.write(fila, 1, '', title_head)
             ws.write(fila, 2, '', title_head)
@@ -148,6 +165,96 @@ class VacationsReport(models.TransientModel):
             ws.write(fila, 8, 0, format_number1) if not total_dias_no_habiles else ws.write(fila, 8, total_dias_no_habiles, format_number1)
             ws.write(fila, 9, 0, format_number1), format_number1 if not total_dias else ws.write(fila, 9, total_dias, format_number1)
             ws.write(fila, 10, 0,format_number1) if not total_dias else ws.write(fila, 10, total_valor_pagado, format_number1)
+
+            # Horas extras promedio 12 meses atras
+            horas_extras_12month_before = self.env['hr.payslip'].get_inputs_hora_extra_12month_before(cont, date_creation, date_creation)
+            extradiurna_amount = 0
+            extradiurnafestivo_amount = 0
+            extranocturna_amount = 0
+            extranocturnafestivo_amount = 0
+            recargonocturno_amount = 0
+            recargodiurnofestivo_amount = 0
+            recargonocturnofestivo_amount = 0
+
+            hm12_date_ini = date_creation - relativedelta(months=12)
+            hm12_date_ini = hm12_date_ini + relativedelta(days=1)
+            if cont.date_start <= hm12_date_ini:
+                hm12_date_init = hm12_date_ini
+            else:
+                hm12_date_init = cont.date_start
+            if date_creation.day == 31:
+                date_creation = date_creation - relativedelta(days=1)
+            total_days12y = days360(hm12_date_init, date_creation) + 1
+
+            for hora in horas_extras_12month_before:
+                if hora[1] == 'EXTRADIURNA':
+                    extradiurna_amount = extradiurna_amount + hora[2]
+                if hora[1] == 'EXTRADIURNAFESTIVO':
+                    extradiurnafestivo_amount = extradiurnafestivo_amount + hora[2]
+                if hora[1] == 'EXTRANOCTURNA':
+                    extranocturna_amount = extranocturna_amount + hora[2]
+                if hora[1] == 'EXTRANOCTURNAFESTIVO':
+                    extranocturnafestivo_amount = extranocturnafestivo_amount + hora[2]
+                if hora[1] == 'RECARGONOCTURNO':
+                    recargonocturno_amount = recargonocturno_amount + hora[2]
+                if hora[1] == 'RECARGODIURNOFESTIVO':
+                    recargodiurnofestivo_amount = recargodiurnofestivo_amount + hora[2]
+                if hora[1] == 'RECARGONOCTURNOFESTIVO':
+                    recargonocturnofestivo_amount = recargonocturnofestivo_amount + hora[2]
+
+            extradiurna12m = (extradiurna_amount / total_days12y) * 30
+            extradiurnafestivo12m = (extradiurnafestivo_amount / total_days12y) * 30
+            extranocturna12m = (extranocturna_amount / total_days12y) * 30
+            extranocturnafestivo12m = (extranocturnafestivo_amount / total_days12y) * 30
+            recargonocturno12m = (recargonocturno_amount / total_days12y) * 30
+            recargodiurnofestivo12m = (recargodiurnofestivo_amount / total_days12y) * 30
+            recargonocturnofestivo12m = (recargonocturnofestivo_amount / total_days12y) * 30
+
+            #ws.write(fila, 11, 0, format_number1) if not total_dias else ws.write(fila, 11, extradiurna12m, format_number1)
+            #ws.write(fila, 12, 0, format_number1) if not total_dias else ws.write(fila, 12, extradiurnafestivo12m, format_number1)
+            #ws.write(fila, 13, 0, format_number1) if not total_dias else ws.write(fila, 13, extranocturna12m, format_number1)
+            #ws.write(fila, 14, 0, format_number1) if not total_dias else ws.write(fila, 14, extranocturnafestivo12m, format_number1)
+            #ws.write(fila, 15, 0, format_number1) if not total_dias else ws.write(fila, 15, recargonocturno12m, format_number1)
+            #ws.write(fila, 16, 0, format_number1) if not total_dias else ws.write(fila, 16, recargodiurnofestivo12m, format_number1)
+            #ws.write(fila, 17, 0, format_number1) if not total_dias else ws.write(fila, 17, recargonocturnofestivo12m, format_number1)
+
+            # Bonificaciones y comisiones  promedio 12 meses atras
+            inputs_loans_12month_before = self.env['hr.payslip'].get_inputs_loans_12month_before(cont, date_creation, date_creation)
+            amountb = 0
+            amountc = 0
+            lm12_date_ini = date_creation - relativedelta(months=12)
+            lm12_date_ini = lm12_date_ini + relativedelta(days=1)
+            if cont.date_start <= lm12_date_ini:
+                lm12_date_init = lm12_date_ini
+            else:
+                lm12_date_init = cont.date_start
+            if date_creation.day == 31:
+                date_creation = date_creation - relativedelta(days=1)
+            total_dayl12 = days360(lm12_date_init, date_creation) + 1
+
+            for loans in inputs_loans_12month_before:
+                if loans[1] == 'BONIFICACION':
+                    amountb = amountb + loans[2]
+                if loans[1] == 'COMISION':
+                    amountc = amountc + loans[2]
+
+            bonificacion12m = (amountb / total_dayl12) * 30
+            comision12m = (amountb / total_dayl12) * 30
+
+            #ws.write(fila, 18, 0, format_number1) if not total_dias else ws.write(fila, 11, bonificacion12m, format_number1)
+            #ws.write(fila, 19, 0, format_number1) if not total_dias else ws.write(fila, 11, comision12m, format_number1)
+
+            salario = cont.wage
+
+            #ws.write(fila, 20, 0, format_number1) if not total_dias else ws.write(fila, 11, salario, format_number1)
+
+            salario12m =  salario+bonificacion12m+comision12m+extradiurna12m+extradiurnafestivo12m+extranocturna12m+extranocturnafestivo12m+recargonocturno12m+recargodiurnofestivo12m+recargonocturnofestivo12m
+
+            #ws.write(fila, 21, 0, format_number1) if not total_dias else ws.write(fila, 11, salario12m, format_number1)
+
+            ws.write(fila, 11, salario12m, format_number1)
+
+
             fila += 1
             ws.write(fila, 0, '')
             fila += 1
