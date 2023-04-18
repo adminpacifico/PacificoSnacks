39430314bc6775ac35478ec9f1c6ce283579f178
jeyko14 <sergio10proyect2.0@gmail.com>
2023-04-17 19:11:34 -0500

    Actualización Comercio exterior
    Campos OP factura de compra

--- foreign_trade/models/account_move.py
+++ foreign_trade/models/account_move.py
@@ -1,4 +1,5 @@
 from odoo import models, fields,api
+from datetime import date, timedelta, datetime
 
 
 class Account_move(models.Model):
@@ -17,9 +18,22 @@ class Account_move(models.Model):
     def compute_sales_order(self):
         for record in self:
             if record.invoice_origin:
-                record.sales_order = self.env['sale.order'].search([('name', '=', record.invoice_origin)])
-                record.export_order = record.sales_order.x_studio_exportacion_
-                record.x_studio_field_3lEMz = record.sales_order.x_studio_exportacion_
+                if record.type == 'out_invoice':
+                    order_origin = self.env['sale.order'].search([('name', '=', record.invoice_origin)])
+                    if order_origin:
+                        record.sales_order = order_origin
+                        record.export_order = record.sales_order.x_studio_exportacion_
+                        record.x_studio_field_3lEMz = record.sales_order.x_studio_exportacion_
+                        if record.sales_order.possible_arrival_date_destination:
+                            if record.invoice_payment_term_id.line_ids:
+                                dias =  timedelta(days=record.invoice_payment_term_id.line_ids.days)
+                            else:
+                                dias = 0
+                            record.x_studio_fecha_de_vencimiento = record.sales_order.possible_arrival_date_destination + dias
+                        else:
+                            record.x_studio_fecha_de_vencimiento = False
+                else:
+                    record.sales_order = False
             else:
                 record.sales_order = False
 #################################################################
