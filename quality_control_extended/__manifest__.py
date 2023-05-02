133f7c588386c5dcd0436759d69b696cf70e2ff1
cquirogap <cesar.linker@gmail.com>
2023-05-02 18:51:27 -0500

    Control de calidad  - Recepcion

---
+++ quality_control_extended/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Control de calidad extendend',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Extender la funcionalidad de control de calidad',
+    'category': 'Category',
+    'author': 'End to End Technology',
+    'website': 'Website',
+    'depends': ['stock', 'quality_control', 'quality'],
+    'data': [
+
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
