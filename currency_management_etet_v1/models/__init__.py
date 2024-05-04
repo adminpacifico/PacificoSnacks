eedaf746aa58da402ddd13ba33de46bb2cf3118f
Cesar Ortiz <certiz@gmail.com>
2024-05-04 17:26:13 -0500

    Delete currency_management_etet_v1 directory

--- currency_management_etet_v1/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import currency_management
-from . import account_move
-from . import account_payment
-from . import account_aged_partner_balance
-#from . import payment
