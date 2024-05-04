64a924a61fb61cf915b8b7dbfc9a809485c5e14e
Cesar Ortiz <certiz@gmail.com>
2024-05-04 17:18:04 -0500

    Delete fiscal_depreciation_report_etet_v1 directory

--- fiscal_depreciation_report_etet_v1/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import fiscal_depreciation_report
-from . import account_asset
-from . import account_move_fiscal
-from . import account_adition_asset
-from . import account_location_asset
-from . import account_asset_sell
-
