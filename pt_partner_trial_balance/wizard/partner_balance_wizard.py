36684eadeea3e5cfbac2ad381f355500f11e8d5c
Cesar Ortiz <certiz@gmail.com>
2024-07-28 15:36:22 -0500

    Update partner_balance_wizard.py

--- pt_partner_trial_balance/wizard/partner_balance_wizard.py
+++ pt_partner_trial_balance/wizard/partner_balance_wizard.py
@@ -21,7 +21,7 @@ class PartnerBalanceWizard(models.TransientModel):
 
     def _prepare_query_conditions(self):
         conditions = [
-            "aml.date < %s",
+            "AND aml.date < %s",
             "aml.company_id = %s"
         ]
         params = [self.start_date, self.company_id.id]
@@ -37,7 +37,7 @@ class PartnerBalanceWizard(models.TransientModel):
 
     def _prepare_transactions_conditions(self):
         conditions = [
-            "aml.date BETWEEN %s AND %s",
+            "AND aml.date BETWEEN %s AND %s",
             "aml.company_id = %s"
         ]
         params = [self.start_date, self.end_date, self.company_id.id]
