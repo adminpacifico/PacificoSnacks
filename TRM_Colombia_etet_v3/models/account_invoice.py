44f6d5d7c0efa8baa9b42350be0ad2533d8173e4
Cesar Ortiz <certiz@gmail.com>
2024-08-14 16:07:06 -0500

    Update account_invoice.py

--- TRM_Colombia_etet_v3/models/account_invoice.py
+++ TRM_Colombia_etet_v3/models/account_invoice.py
@@ -9,8 +9,9 @@ class AccountMove(models.Model):
     @api.onchange('invoice_date', 'currency_id')
     def compute_trm(self):
         for record in self:
-            rates = self.env["res.currency.rate"].sudo().search([("name", "=", record.invoice_date), ("currency_id", "=", record.currency_id.id)])
-            for tasas in rates:
-                if tasas.x_studio_field_rqbWr > 0:
-                    record.trm = tasas.x_studio_field_rqbWr
+            if record.currency_id.name != "COP":
+                rates = self.env["res.currency.rate"].sudo().search([("name", "=", record.invoice_date), ("currency_id", "=", record.currency_id.id)])
+                for tasas in rates:
+                    if tasas.x_studio_field_rqbWr > 0:
+                        record.trm = tasas.x_studio_field_rqbWr
 
