a3a8e46f28eca474df144b2183197cc0ac3b6f4b
Cesar Ortiz <certiz@gmail.com>
2024-10-10 18:55:10 -0500

    Update __manifest__.py

--- Font_Ketchup/__manifest__.py
+++ Font_Ketchup/__manifest__.py
@@ -2,11 +2,12 @@
 	'name': 'Font_Tomato',
 	'author': 'Para Ti',
 	'category': '',
-	'version': '',
+	'version': '17.0',
 	'depends': [],
 	'data': '',
 	'assets': {
 		'web.report_assets_common': ['/Font_Ketchup/static/src/css/font.css']	
 	},
 	'aplication': True,
+	'installable': True,
 }
