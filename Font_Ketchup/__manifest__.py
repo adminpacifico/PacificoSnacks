26c9465bb1b2927ba76cdad133349318573767db
laom300 <111718063+laom300@users.noreply.github.com>
2024-10-10 18:49:49 -0500

    Font

---
+++ Font_Ketchup/__manifest__.py
@@ -0,0 +1,12 @@
+{
+	'name': 'Font_Tomato',
+	'author': 'Para Ti',
+	'category': '',
+	'version': '',
+	'depends': [],
+	'data': '',
+	'assets': {
+		'web.report_assets_common': ['/Font_Ketchup/static/src/css/font.css']	
+	},
+	'aplication': True,
+}
